21dee894d03bfe7e0dbf6fccd068f7c93d91f6c7
Christian Perez <perezchristian1012@gmail.com>
2019-09-15 20:00:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,14 +1,14 @@
 class Triangle
-  
+
   attr_accessor :side_one, :side_two, :side_three
-  
+
   def initialize(side_one, side_two, side_three)
       @side_one = side_one
       @side_two = side_two
       @side_three = side_three
   end
-  
-  def kind 
+
+  def kind
     if @side_one <= 0 || @side_two <= 0 || @side_three <= 0 || @side_one + @side_two <= @side_three || @side_three + @side_two <= @side_one || @side_one + @side_three <= @side_two
       begin
         raise TriangleError
@@ -21,11 +21,11 @@ class Triangle
       :scalene
     end
   end
-  
+
   class TriangleError < StandardError
     def message
       "Invalid triangle."
     end
   end
-  
+
 end
