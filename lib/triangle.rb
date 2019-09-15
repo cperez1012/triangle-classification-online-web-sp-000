d722d54b6b49aca52182428148419a208eb288a2
Christian Perez <perezchristian1012@gmail.com>
2019-09-15 19:56:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,4 +7,24 @@ class Triangle
     @length_2 = length_2
     @length_3 = length_3
   end
+
+  def kind 
+    if length_1 <= 0 || length_2 <= 0 || length_3 <= 0 || length_1 + length_2 <= length_3 || length_3 + length_2 <= length_1 || length_1 + length_3 <= length_2
+      begin
+        raise TriangleError
+      end
+    elsif length_1 == length_2 && length_2 == length_3
+      :equilateral
+    elsif length_1 == length_2 || length_2 == length_3 || length_1 == length_3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  
+  class TriangleError < StandardError
+    def message
+      "Invalid triangle."
+    end
+  end
 end
