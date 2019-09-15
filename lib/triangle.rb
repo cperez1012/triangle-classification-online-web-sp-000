1bdb4df26fc2b4c8e29e227fd0fa94c859451c33
Christian Perez <perezchristian1012@gmail.com>
2019-09-15 20:00:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,30 +1,31 @@
 class Triangle
-
-  attr_accessor :length_1 :length_2, :length_3
-
-  def initialize(length_1, length_2, length_3)
-    @length_1 = length_1
-    @length_2 = length_2
-    @length_3 = length_3
+  
+  attr_accessor :side_one, :side_two, :side_three
+  
+  def initialize(side_one, side_two, side_three)
+      @side_one = side_one
+      @side_two = side_two
+      @side_three = side_three
   end
-
-  def kind
-    if length_1 <= 0 || length_2 <= 0 || length_3 <= 0 || length_1 + length_2 <= length_3 || length_3 + length_2 <= length_1 || length_1 + length_3 <= length_2
+  
+  def kind 
+    if @side_one <= 0 || @side_two <= 0 || @side_three <= 0 || @side_one + @side_two <= @side_three || @side_three + @side_two <= @side_one || @side_one + @side_three <= @side_two
       begin
         raise TriangleError
       end
-    elsif length_1 == length_2 && length_2 == length_3
+    elsif @side_one == @side_two && @side_two == @side_three
       :equilateral
-    elsif length_1 == length_2 || length_2 == length_3 || length_1 == length_3
+    elsif @side_one == @side_two || @side_two == @side_three || @side_one == @side_three
       :isosceles
     else
       :scalene
     end
   end
-
+  
   class TriangleError < StandardError
     def message
       "Invalid triangle."
     end
   end
+  
 end
