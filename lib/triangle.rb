22bc548a3ffd4eabf4de82cef74f9840094b95e5
Christian Perez <perezchristian1012@gmail.com>
2019-09-15 20:06:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,21 +1,21 @@
 class Triangle
 
-  attr_accessor :side_one, :side_two, :side_three
+  attr_accessor :length_one, :length_two, :length_three
 
-  def initialize(side_one, side_two, side_three)
-      @side_one = side_one
-      @side_two = side_two
-      @side_three = side_three
+  def initialize(length_one, length_two, length_three)
+      @length_one = length_one
+      @length_two = length_two
+      @length_three = length_three
   end
 
   def kind
-    if @side_one <= 0 || @side_two <= 0 || @side_three <= 0 || @side_one + @side_two <= @side_three || @side_three + @side_two <= @side_one || @side_one + @side_three <= @side_two
+    if length_one <= 0 || length_two <= 0 || length_three <= 0 || length_one + length_two <= length_three || length_three + length_two <= length_one || length_one + length_three <= length_two
       begin
         raise TriangleError
       end
-    elsif @side_one == @side_two && @side_two == @side_three
+    elsif length_one == length_two && length_two == length_three
       :equilateral
-    elsif @side_one == @side_two || @side_two == @side_three || @side_one == @side_three
+    elsif length_one == length_two || length_two == length_three || length_one == length_three
       :isosceles
     else
       :scalene
